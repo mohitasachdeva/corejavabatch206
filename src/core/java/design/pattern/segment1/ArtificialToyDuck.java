aeb7a896acfa977c299cdec0c700be8250a09f70
HP <sachdevamohita5@gmail.com>
2023-01-04 14:47:41 +0530

    Adapter Dp

---
+++ src/core/java/design/pattern/segment1/ArtificialToyDuck.java
@@ -0,0 +1,8 @@
+package core.java.design.pattern.segment1;
+
+public class ArtificialToyDuck implements  ToyDuck{
+    @Override
+    public void squeak() {
+        System.out.println("Squeak");
+    }
+}
---
+++ src/core/java/design/pattern/segment1/MAinAdapter.java
@@ -0,0 +1,25 @@
+package core.java.design.pattern.segment1;
+
+public class MAinAdapter {
+    public static void main(String[] args)
+            {
+                Sparrow sparrow = new Sparrow();
+                ToyDuck toyDuck = new ArtificialToyDuck();
+
+                // Wrap a bird in a birdAdapter so that it
+                // behaves like toy duck
+                ToyDuck birdAdapter = new Adapter(sparrow);
+
+                System.out.println("Sparrow...");
+                sparrow.fly();
+                sparrow.makeSound();
+
+                System.out.println("ToyDuck...");
+                toyDuck.squeak();
+
+                // toy duck behaving like a bird
+                System.out.println("BirdAdapter...");
+                birdAdapter.squeak();
+            }
+        }
+
