3c51ae163124b56a6e4dc4e2bf387cd4cbed5d2e
HP <sachdevamohita5@gmail.com>
2023-01-08 18:22:29 +0530

    Reflection

---
+++ src/core/java/design/pattern/segment1/reflection/Reflection.java
@@ -0,0 +1,26 @@
+package core.java.design.pattern.segment1.reflection;
+
+import java.lang.reflect.Method;
+
+public class Reflection {
+    public void get(){
+        System.out.println("hello world");
+    }
+    public void display(){
+        System.out.println("i am learnning core java");
+    }
+}
+class ReflectionUSe {
+    public static void main(String[] args) {
+        Reflection reflection = new Reflection();
+        Class aclass = reflection.getClass();
+        String c =  aclass.getName();
+        System.out.println("name of the class"+c);
+        Method c1[] =  aclass.getDeclaredMethods();
+        for (Method m: c1
+        ) {
+            System.out.println("name of the methods that declare in class"+m.getName());
+        }
+
+    }
+}
